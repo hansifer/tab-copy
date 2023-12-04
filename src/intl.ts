bd471780fdf75d7aaf3d2af07aeedaabbe2ae83c
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-04 10:46:47 -0700

    add intl

---
+++ src/intl.ts
@@ -0,0 +1,57 @@
+export const intl = {
+  // ----- copying  -----
+
+  copy: () => 'copy',
+
+  copyAs: () => 'copy as',
+
+  copy1xAs: () => 'copy 1x as',
+
+  holdWhenCopying: (modifierKeys: string) => `hold ${modifierKeys} to use when copying`,
+
+  // ----- copy scope -----
+
+  thisTab: () => 'this tab',
+
+  thisWindowsTabs: () => "this window's tabs",
+
+  allTabs: () => 'all tabs',
+
+  // ----- copy format -----
+
+  link: () => 'link',
+
+  title: () => 'title',
+
+  url: () => 'URL',
+
+  htmlTable: () => 'HTML table',
+
+  primary: () => 'primary',
+
+  // ----- options -----
+
+  confirmCopyWithPopup: () => 'confirm copy with popup',
+
+  ignorePinnedTabs: () => 'ignore pinned tabs',
+
+  showInContextMenu: () => 'show in context menu',
+
+  grayscaleIcon: () => 'grayscale icon',
+
+  shortcutKey: () => 'shortcut key',
+
+  // ----- modifier keys -----
+
+  alt: () => 'alt',
+
+  ctrl: () => 'ctrl',
+
+  shift: () => 'shift',
+
+  modifierKeys: (...keys: string[]) => keys.join('+'),
+
+  // ----- misc -----
+
+  conjoin: (...items: string[]) => items.join(' & '),
+}
