0020420300bffd454f472b319f8319edde5b7b81
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-05 12:29:51 -0700

    add options module

---
+++ src/options.ts
@@ -0,0 +1,10 @@
+// --- add new options below by way of declaring defaults ---
+
+export const options = {
+  confirmCopyWithPopup: false,
+  ignorePinnedTabs: false,
+  showInContextMenu: true,
+  grayscaleIcon: false,
+}
+
+export type OptionId = keyof typeof options
