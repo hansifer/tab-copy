6a28c7f5037955f44c74af778ce94f330ef497c7
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-05 17:27:00 -0700

    apply grayscaleIcon option

--- src/background.ts
+++ src/background.ts
@@ -4,7 +4,13 @@ let actionIconFlashTimer: ReturnType<typeof setTimeout>
 
 console.log(`service worker loaded ${new Date().toLocaleString()}`)
 
-// listening to `copied` storage event instead of using `chrome.runtime` messaging because `window.close()` interrupts the message
+overrideIcon()
+
+chrome.runtime.onStartup.addListener(() => {
+  overrideIcon()
+})
+
+// listening for `copied` storage event instead of using `chrome.runtime` messaging because `window.close()` interrupts the message
 chrome.storage.onChanged.addListener(
   makeStorageChangeHandler((changes) => {
     if (changes.copied) {
@@ -16,15 +22,28 @@ chrome.storage.onChanged.addListener(
         setIcon('logo')
       }, 1e3)
     }
+
+    if (changes.grayscaleIcon) {
+      setIconPaths(changes.grayscaleIcon.newValue ? 'logo-gray' : 'logo')
+    }
   }),
 )
 
-async function setIcon(name: 'success' | 'logo') {
+async function overrideIcon() {
+  const grayscaleIcon = await getOption('grayscaleIcon')
+  if (grayscaleIcon) setIconPaths('logo-gray')
+}
+
+async function setIcon(name: 'logo' | 'success') {
   const filename =
     name === 'logo' && (await getOption('grayscaleIcon')) // wrap
       ? 'logo-gray'
       : name
 
+  return setIconPaths(filename)
+}
+
+function setIconPaths(filename: 'logo' | 'logo-gray' | 'success') {
   return chrome.action.setIcon({
     path: {
       16: `img/${filename}-16.png`,
