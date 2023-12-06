4146e9a5beeb40127c9330b4f11123c31bc5b7f0
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-05 19:19:03 -0700

    wire up to storage

--- src/Options/BinaryOption.tsx
+++ src/Options/BinaryOption.tsx
@@ -1,16 +1,52 @@
+import { useState, useEffect, useCallback } from 'react'
+
 import { Checkbox } from './Checkbox'
+import { getOptionLabel, BooleanOptionId } from '@/options'
+import { getOption, setOption, makeStorageChangeHandler } from '@/storage'
+import { sentenceCase } from '@/util/string'
 
 import classes from './BinaryOption.module.css'
 
 type BinaryOptionProps = {
-  label: string
+  id: BooleanOptionId
 }
 
-export const BinaryOption = ({ label }: BinaryOptionProps) => {
+// todo: consider useSyncExternalStore instead of useState, useEffect
+export const BinaryOption = ({ id }: BinaryOptionProps) => {
+  const [checked, setChecked] = useState<boolean>(false)
+
+  useEffect(() => {
+    getOption(id).then(setChecked)
+  }, [id])
+
+  useEffect(() => {
+    const handleStorageChanged = makeStorageChangeHandler((changes) => {
+      if (changes[id]) setChecked(changes[id].newValue)
+    })
+
+    chrome.storage.onChanged.addListener(handleStorageChanged)
+
+    return () => {
+      chrome.storage.onChanged.removeListener(handleStorageChanged)
+    }
+  }, [id])
+
+  const handleClick = useCallback(() => {
+    setOption(id, !checked)
+  }, [id, checked])
+
   return (
     <div className={classes.BinaryOption}>
-      <Checkbox />
-      <span>{label}</span>
+      <Checkbox
+        checked={checked}
+        onClick={handleClick}
+      />
+      <span
+        className={classes.label}
+        onClick={handleClick}
+      >
+        {sentenceCase(getOptionLabel(id))}
+      </span>
     </div>
   )
 }
