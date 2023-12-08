0a0b0a5455400760f31af67d418a8d612556a14d
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-08 12:43:12 -0700

    make ternary action key modifier shift only; change secondary action key modifier label to ctrl

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -18,18 +18,8 @@ export function hasSecondaryActionKeyModifier({
   return !shiftKey && hasPrincipalKeyModifier({ metaKey, ctrlKey, altKey })
 }
 
-export function hasTernaryActionKeyModifier({
-  metaKey,
-  ctrlKey,
-  altKey,
-  shiftKey,
-}: {
-  metaKey: boolean
-  ctrlKey: boolean
-  altKey: boolean
-  shiftKey: boolean
-}) {
-  return shiftKey && hasPrincipalKeyModifier({ metaKey, ctrlKey, altKey })
+export function hasTernaryActionKeyModifier({ shiftKey }: { shiftKey: boolean }) {
+  return shiftKey
 }
 
 // the "principal" key modifier is either cmd (if mac) or ctrl/alt (if windows/linux)
@@ -46,11 +36,9 @@ function hasPrincipalKeyModifier({
 }
 
 export function getSecondaryActionKeyModifierLabel() {
-  return clientInfo.os === 'mac' ? '⌘' : sentenceCase(intl.alt())
+  return clientInfo.os === 'mac' ? '⌘' : sentenceCase(intl.ctrl())
 }
 
 export function getTernaryActionKeyModifierLabel() {
-  return clientInfo.os === 'mac'
-    ? '⌘+⇧'
-    : intl.keyModifier(sentenceCase(intl.alt()), sentenceCase(intl.shift()))
+  return clientInfo.os === 'mac' ? '⇧' : sentenceCase(intl.shift())
 }
