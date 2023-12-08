35eb75c713fc1298395998e75bd363a237c6f439
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-08 09:21:42 -0700

    add support for custom formats; separate out configured format concerns

--- src/formats.ts
+++ src/formats.ts
@@ -1,18 +1,31 @@
-import {
-  getOrderedFormatIds,
-  getSelectableFormatIds,
-  getPrimaryFormatId,
-  getFormatOpts,
-} from '@/storage'
+import { getFormatOption } from '@/storage'
 import { intl } from '@/intl'
 import { sentenceCase } from '@/util/string'
+
+export type BuiltInFormatId = (typeof builtInFormatIds)[number]
+export type CustomFormatId = `custom-${string}`
+export type FormatId = BuiltInFormatId | CustomFormatId
+
+export type BuiltInFormatOptions = typeof builtinFormatOptions
+export type CustomFormatOption = typeof customFormatOption
+
+export type BuiltInFormatWithOptionId = keyof BuiltInFormatOptions
+export type BuiltInFormatWithoutOptionId = Exclude<BuiltInFormatId, BuiltInFormatWithOptionId>
+
+export type FormatOptions = {
+  [k in BuiltInFormatWithOptionId]: BuiltInFormatOptions[k]
+} & {
+  [k in BuiltInFormatWithoutOptionId]?: undefined
+} & {
+  [k: CustomFormatId]: CustomFormatOption
+}
 
 // require a minimum number of selectable formats
 export const MIN_SELECTABLE_FORMAT_COUNT = 3
 
-// --- 1. add new static format id below ---
+// --- 1. add new built-in format id below ---
 
-export const formatIds = [
+export const builtInFormatIds = [
   'link',
   'url',
   'titleUrl1Line',
@@ -24,21 +37,24 @@ export const formatIds = [
   'json',
   'html',
   'htmlTable',
-  'custom',
 ] as const
 
-export type FormatId = (typeof formatIds)[number]
+export function getFormatLabel(id: FormatId) {
+  return isBuiltInFormatId(id) // wrap
+    ? getBuiltInFormatLabel(id)
+    : getCustomFormatLabel(id)
+}
 
-// --- 2. add new static format id-to-label mapping below ---
+// --- 2. add new built-in format id-to-label mapping below ---
 
-export async function getFormatLabel(id: FormatId): Promise<string> {
+async function getBuiltInFormatLabel(id: BuiltInFormatId): Promise<string> {
   switch (id) {
     case 'link':
       return sentenceCase(intl.link())
     case 'url':
       return intl.url()
     case 'titleUrl1Line':
-      const { separator } = (await getFormatOpts('titleUrl1Line'))!
+      const { separator } = await getFormatOption('titleUrl1Line')
       return `${sentenceCase(intl.title())}${separator}${sentenceCase(intl.url())}`
     case 'titleUrl2Line':
       return intl.conjoin(sentenceCase(intl.title()), sentenceCase(intl.url()))
@@ -56,75 +72,66 @@ export async function getFormatLabel(id: FormatId): Promise<string> {
       return 'HTML'
     case 'htmlTable':
       return sentenceCase(intl.htmlTable())
-    case 'custom':
-      return 'Custom' // not used
   }
 }
 
-// --- 3. add new format opts, if any, below by way of declaring defaults ---
+async function getCustomFormatLabel(id: CustomFormatId): Promise<string> {
+  const { name } = await getFormatOption(id)
+  return name
+}
 
-export const formatOpts = {
+// --- 3. add new built-in format option, if any, below by way of declaring defaults ---
+
+export const builtinFormatOptions = {
   titleUrl1Line: {
     separator: ': ',
   },
   htmlTable: {
     includeHeader: false,
   },
-  custom: {
-    name: 'My format',
-    template: {
-      header: '[date][n][n]',
-      tab: '[#]) Title: [title][n]   URL:   [url]',
-      delimiter: '[n][n]',
-      footer: '',
-    },
+} satisfies { [k in BuiltInFormatId]?: Record<string, any> }
+
+export const customFormatOption = {
+  name: 'My format',
+  template: {
+    header: '[date][n][n]',
+    tab: '[#]) Title: [title][n]   URL:   [url]',
+    delimiter: '[n][n]',
+    footer: '',
   },
-} satisfies { [k in FormatId]?: Record<string, any> }
-
-export function formatHasOpts(id: FormatId): id is keyof typeof formatOpts {
-  return id in formatOpts
 }
 
-export type ConfiguredFormat = {
-  id: FormatId
-  label: string
-  selectable?: boolean
-  primary?: boolean
-  opts?: (typeof formatOpts)[keyof typeof formatOpts] | null
-}
+// select format option from lookup with default fallback
+export function selectFormatOption<T extends FormatId>(
+  id: T,
+  lookup?: Partial<FormatOptions>,
+): FormatOptions[T] {
+  if (lookup?.[id]) return lookup[id]! // TS can't figure out that `lookup[id]` is defined
 
-export async function getConfiguredFormat(id: FormatId): Promise<ConfiguredFormat> {
-  const selectableFormatIds = await getSelectableFormatIds()
-  const primaryFormatId = await getPrimaryFormatId()
+  // fallback
 
-  return createConfiguredFormat(id, selectableFormatIds, primaryFormatId)
-}
-
-export async function getConfiguredFormats({
-  selectableOnly,
-}: { selectableOnly?: boolean } = {}): Promise<ConfiguredFormat[]> {
-  const selectableFormatIds = await getSelectableFormatIds()
-  const primaryFormatId = await getPrimaryFormatId()
-
-  const formatIds = selectableOnly // wrap
-    ? selectableFormatIds
-    : await getOrderedFormatIds()
-
-  return Promise.all(
-    formatIds.map((id) => createConfiguredFormat(id, selectableFormatIds, primaryFormatId)),
-  )
-}
-
-async function createConfiguredFormat(
-  id: FormatId,
-  selectableFormatIds: FormatId[],
-  primaryFormatId: FormatId,
-): Promise<ConfiguredFormat> {
-  return {
-    id,
-    label: await getFormatLabel(id),
-    selectable: selectableFormatIds.includes(id),
-    primary: primaryFormatId === id,
-    opts: await getFormatOpts(id),
+  if (isBuiltInFormatWithOptionId(id)) {
+    return builtinFormatOptions[id]
   }
+
+  if (isCustomFormatId(id)) {
+    return customFormatOption as FormatOptions[T] // todo: why is assertion necessary?
+  }
+
+  return undefined as FormatOptions[T] // todo: why is assertion necessary?
+}
+
+// user-defined type guards
+
+export function isBuiltInFormatId(id: FormatId): id is BuiltInFormatId {
+  // https://stackoverflow.com/a/56745484/384062
+  return (builtInFormatIds as ReadonlyArray<string>).includes(id)
+}
+
+export function isBuiltInFormatWithOptionId(id: FormatId): id is BuiltInFormatWithOptionId {
+  return id in builtinFormatOptions
+}
+
+export function isCustomFormatId(id: FormatId): id is CustomFormatId {
+  return id.startsWith('custom-')
 }
