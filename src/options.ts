cb17dc06c4f11c62eeb8767d7f08198431b9a870
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-11 12:34:42 -0700

    make selectOption() generic

--- src/options.ts
+++ src/options.ts
@@ -21,6 +21,6 @@ export function getOptionLabel(id: OptionId) {
 }
 
 // select option value with default fallback
-export function selectOption(id: OptionId, lookup?: Partial<Options>) {
+export function selectOption<T extends OptionId>(id: T, lookup?: Partial<Options>) {
   return lookup?.[id] ?? options[id]
 }
