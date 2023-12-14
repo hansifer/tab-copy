208fb5061bbc1f345a705475e0337ff47be219ac
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-14 12:47:29 -0700

    add copy module

---
+++ src/copy.ts
@@ -0,0 +1,11 @@
+import { FormatId } from '@/format'
+import { ConfiguredFormat } from '@/configured-format'
+import { intl } from '@/intl'
+
+export async function copyTabs(tabs: chrome.tabs.Tab[], format: ConfiguredFormat<FormatId>) {
+  // throw new Error('test')
+  console.log(`copying ${tabs.length} ${intl.tab(tabs.length)} as ${format.label}`)
+
+  // todo: replace with formatted copy implementation
+  await navigator.clipboard.writeText(JSON.stringify(tabs.map(({ title }) => title)))
+}
