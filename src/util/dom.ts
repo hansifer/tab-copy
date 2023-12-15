570cdd0586e7624b00e246c95c037d7c7f508d5b
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-15 12:51:23 -0700

    add addListener()

--- src/util/dom.ts
+++ src/util/dom.ts
@@ -1,3 +1,12 @@
 export function isButton(el: any): el is HTMLButtonElement {
   return HTMLButtonElement.prototype.isPrototypeOf(el)
 }
+
+// add a listener to multiple elements
+export function addListener<T extends HTMLElement[], U extends keyof HTMLElementEventMap>(
+  els: T,
+  type: U,
+  listener: (this: T[number], ev: HTMLElementEventMap[U]) => any,
+) {
+  els.forEach((el) => el.addEventListener(type, listener))
+}
