574ae82d2a00e321d41af6f8073dc4b4505a376a
Hans Meyer <hansifer@users.noreply.github.com>
2023-12-15 13:03:41 -0700

    allow addListener() to accept multiple types

--- src/util/dom.ts
+++ src/util/dom.ts
@@ -5,8 +5,12 @@ export function isButton(el: any): el is HTMLButtonElement {
 // add a listener to multiple elements
 export function addListener<T extends HTMLElement[], U extends keyof HTMLElementEventMap>(
   els: T,
-  type: U,
+  type: U | U[],
   listener: (this: T[number], ev: HTMLElementEventMap[U]) => any,
 ) {
-  els.forEach((el) => el.addEventListener(type, listener))
+  const types = Array.isArray(type) ? type : [type]
+
+  types.forEach((type) => {
+    els.forEach((el) => el.addEventListener(type, listener))
+  })
 }
