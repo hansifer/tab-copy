4bbbb9b48b13397b173babf4bbca86cecf7fcfa8
Hans Meyer <hansifer@users.noreply.github.com>
2024-07-08 17:35:24 -0600

    position label vertically; change width prop to string; adjust label font and color

--- src/options/TextOption/TextOption.tsx
+++ src/options/TextOption/TextOption.tsx
@@ -1,16 +1,14 @@
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { sentenceCase } from '@/util/string'
 import classes from './TextOption.module.css'
 
 // todo: add validation feature
 
-const LEFT_ALIGN_THRESHOLD = 0.6
-
 type TextOptionProps = {
   label: string
   value: string
-  width?: number
+  width?: string
   maxLength?: number
   autoFocus?: boolean
   onChange: (value: string) => void
@@ -20,14 +18,11 @@ export const TextOption = ({
   // wrap
   label,
   value,
-  width,
+  width = '100%',
   maxLength,
   autoFocus,
   onChange,
 }: TextOptionProps) => {
-  const [inputAlignLeft, setInputAlignLeft] = useState(false)
-
-  const textOptionRef = useRef<HTMLDivElement>(null)
   const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
@@ -36,29 +31,15 @@ export const TextOption = ({
     }
   }, [autoFocus])
 
-  useEffect(() => {
-    setInputAlignLeft(
-      !!(
-        inputRef.current &&
-        textOptionRef.current &&
-        inputRef.current.offsetWidth / textOptionRef.current.offsetWidth < LEFT_ALIGN_THRESHOLD
-      ),
-    )
-  }, [setInputAlignLeft, width]) // todo: using width in dep array to trigger effect on width change, against recommendations (avoid using dep array for control flow)
-
   return (
     <div
-      ref={textOptionRef}
       className={classes.TextOption}
-      style={{
-        justifyContent: inputAlignLeft ? 'flex-start' : 'space-between',
-      }}
+      style={{ width }}
     >
-      <div className={classes.label}>{sentenceCase(label)}:</div>
+      <div className={classes.label}>{sentenceCase(label)}</div>
       <input
         ref={inputRef}
         type="text"
-        style={{ width: width ? `${width}px` : '80%' }}
         maxLength={maxLength}
         onInput={({ currentTarget }) => {
           onChange(currentTarget.value)
