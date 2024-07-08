938e1e755c8142ef634c477b009f79fca4be3aef
Hans Meyer <hansifer@users.noreply.github.com>
2024-07-08 11:01:54 -0600

    quote logged format label

--- src/copy.ts
+++ src/copy.ts
@@ -4,7 +4,7 @@ import { intl } from '@/intl'
 
 export async function copyTabs(tabs: chrome.tabs.Tab[], format: ConfiguredFormat<FormatId>) {
   // throw new Error('test')
-  console.log(`copying ${tabs.length} ${intl.tab(tabs.length)} as ${format.label}`)
+  console.log(`copying ${tabs.length} ${intl.tab(tabs.length)} as "${format.label}"`)
 
   // todo: replace with formatted copy implementation
   await navigator.clipboard.writeText(JSON.stringify(tabs.map(({ title }) => title)))
