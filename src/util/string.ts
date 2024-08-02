a18eabd8cc678121b9fc9a4788cb9f32f2d723e8
Hans Meyer <hansifer@users.noreply.github.com>
2024-08-02 15:25:06 -0600

    add indent(), encodeHtml()

--- src/util/string.ts
+++ src/util/string.ts
@@ -3,3 +3,17 @@ export function sentenceCase(text?: string): string {
 
   return `${text[0].toLocaleUpperCase()}${text.substring(1)}`
 }
+
+export function indent(text: string, indent = 2) {
+  return text.replace(/^/gm, ' '.repeat(indent))
+}
+
+// https://stackoverflow.com/a/11561642/384062
+export function encodeHtml(html: string) {
+  return html
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/'/g, '&#39;')
+    .replace(/"/g, '&#34;')
+}
