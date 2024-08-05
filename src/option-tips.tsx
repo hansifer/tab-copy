671e0ca6a48f40ae792ffb2bb316c6bfaddc4213
Hans Meyer <hansifer@users.noreply.github.com>
2024-08-05 13:37:20 -0600

    add scope option tips

--- src/option-tips.tsx
+++ src/option-tips.tsx
@@ -5,11 +5,32 @@ type OptionTip = {
   icon: JSX.Element
 }
 
+const hiddenSvg = (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
+    <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
+    <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
+    <line
+      x1="2"
+      x2="22"
+      y1="2"
+      y2="22"
+    />
+  </svg>
+)
+
 // --- 1. add new option tip below ---
 
-export const formatOptionTips = [
+export const scopeOptionTips = [
   {
-    id: 'hidden-formats',
+    id: 'copy-button-scopes',
     icon: (
       <svg
         viewBox="0 0 24 24"
@@ -19,18 +40,24 @@ export const formatOptionTips = [
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
-        <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
-        <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
+        <polyline points="18 8 22 12 18 16" />
+        <polyline points="6 8 2 12 6 16" />
         <line
           x1="2"
           x2="22"
-          y1="2"
-          y2="22"
+          y1="12"
+          y2="12"
         />
       </svg>
     ),
   },
+  {
+    id: 'hidden-scopes',
+    icon: hiddenSvg,
+  },
+] as const satisfies OptionTip[]
+
+export const formatOptionTips = [
   {
     id: 'format-order',
     icon: (
@@ -64,10 +91,15 @@ export const formatOptionTips = [
       </svg>
     ),
   },
+  {
+    id: 'hidden-formats',
+    icon: hiddenSvg,
+  },
 ] as const satisfies OptionTip[]
 
 export const optionTips = [
   // ...generalOptionTips, // if needed
+  ...scopeOptionTips,
   ...formatOptionTips,
 ] as const
 
