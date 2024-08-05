cc852216d348a3798b4ac9ddb8935a351c28a8f6
Hans Meyer <hansifer@users.noreply.github.com>
2024-08-05 12:34:58 -0600

    add label and description to scope defs

--- src/scope.ts
+++ src/scope.ts
@@ -1,10 +1,42 @@
+import { intl } from '@/intl'
+
 export const MIN_VISIBLE_SCOPE_COUNT = 1
 
-export const scopeIds = [
-  'highlighted-tabs',
-  'window-tabs',
-  'all-tabs',
-  'all-windows-and-tabs',
-] as const
+export type Scope = (typeof scopes)[number]
+export type ScopeId = Scope['id']
 
-export type ScopeId = (typeof scopeIds)[number]
+export const scopes = [
+  {
+    id: 'highlighted-tabs',
+    label: ({ highlightedTabCount = 0 } = {}) =>
+      highlightedTabCount > 1 // wrap
+        ? intl.selectedTabs()
+        : intl.thisTab(),
+    description: () => intl.thisTabDescription(),
+  },
+  {
+    id: 'window-tabs',
+    label: () => intl.thisWindowsTabs(),
+    description: () => intl.thisWindowsTabsDescription(),
+  },
+  {
+    id: 'all-tabs',
+    label: () => intl.allTabs(),
+    description: () => intl.allTabsDescription(),
+  },
+  {
+    id: 'all-windows-and-tabs',
+    label: () => intl.allWindowsAndTabs(),
+    description: () => intl.allWindowsAndTabsDescription(),
+  },
+] as const satisfies ScopeTemplate[]
+
+type ScopeTemplate = {
+  id: string
+  label: (tabsInfo?: TabsInfo) => string
+  description?: () => string
+}
+
+type TabsInfo = {
+  highlightedTabCount?: number // count of highlighted tabs in the current window
+}
