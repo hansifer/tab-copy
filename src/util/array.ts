a05509fb7c870273d36c5090280af19d6d590ebb
Hans Meyer <hansifer@users.noreply.github.com>
2024-08-18 14:02:03 -0600

    add addOrRemove()

--- src/util/array.ts
+++ src/util/array.ts
@@ -1,3 +1,11 @@
 export function unique<T>(items: T[]): T[] {
   return items.filter((item, i) => items.indexOf(item) === i)
 }
+
+export function addOrRemove<T>(items: T[], item: T): T[] {
+  const exists = items.includes(item)
+
+  return exists // wrap
+    ? items.filter((i) => i !== item)
+    : [...items, item]
+}
