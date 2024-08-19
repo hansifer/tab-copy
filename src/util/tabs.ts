7a68e1e05fce372d7b25c5ad6d98e3e2babd80f9
Hans Meyer <hansifer@users.noreply.github.com>
2024-08-19 09:10:36 -0600

    change getTabs() to return tabs for a particular scope; add tab filtering; add getWindowsAndTabs()

--- src/util/tabs.ts
+++ src/util/tabs.ts
@@ -1,15 +1,35 @@
-// todo: filter per "Ignore pinned tabs" option
-export async function getTabs() {
-  const wins = await chrome.windows.getAll({ populate: true })
-  const currentWinId = (await chrome.windows.getCurrent()).id
+import { TabScopeId } from '@/scope'
 
-  const all = wins.flatMap(({ tabs }) => tabs).filter((tab): tab is chrome.tabs.Tab => !!tab)
-  const inCurrentWin = all.filter(({ windowId }) => windowId === currentWinId)
-  const highlighted = inCurrentWin.filter(({ highlighted }) => !!highlighted)
+type TabPredicate = (tab: chrome.tabs.Tab) => boolean
 
-  return {
-    highlighted,
-    inCurrentWin,
-    all,
+export async function getWindowsAndTabs(filter?: TabPredicate) {
+  const windows = await chrome.windows.getAll({ populate: true })
+
+  if (!filter) return windows
+
+  return windows
+    .map((win) => ({
+      ...win,
+      tabs: (win.tabs ?? []).filter((tab) => filter(tab)),
+    }))
+    .filter(({ tabs }) => tabs.length)
+}
+
+export async function getTabs(scopeId: TabScopeId = 'all-tabs', filter?: TabPredicate) {
+  const windows = await getWindowsAndTabs(filter)
+  const allTabs = windows.flatMap(({ tabs }) => tabs).filter((tab) => !!tab)
+
+  if (scopeId === 'all-tabs') {
+    return allTabs
   }
+
+  const currentWinId = (await chrome.windows.getCurrent()).id
+  const windowTabs = allTabs.filter(({ windowId }) => windowId === currentWinId)
+
+  if (scopeId === 'window-tabs') {
+    return windowTabs
+  }
+
+  // scopeId === 'highlighted-tabs'
+  return windowTabs.filter(({ highlighted }) => !!highlighted)
 }
