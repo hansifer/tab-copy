eec394e14aea759a039f3a3f426f0978bd857780
Hans Meyer <hansifer@users.noreply.github.com>
2024-08-25 16:21:24 -0600

    loosen minimum_chrome_version; remove suggested_key for default command; add content_security_policy

--- src/manifest.ts
+++ src/manifest.ts
@@ -6,7 +6,7 @@ export default defineManifest({
   version: pkg.version,
   description: 'Quickly copy tabs to the clipboard in a variety of formats',
   manifest_version: 3,
-  minimum_chrome_version: '109',
+  minimum_chrome_version: '90',
   icons: {
     16: 'img/logo-16.png',
     32: 'img/logo-32.png',
@@ -25,14 +25,7 @@ export default defineManifest({
   permissions: ['tabs', 'contextMenus', 'storage'],
   optional_permissions: ['notifications'],
   // optional_host_permissions: ['file:///*'],
-  commands: {
-    _execute_action: {
-      suggested_key: {
-        windows: 'Alt+K',
-        mac: 'Command+K',
-        chromeos: 'Alt+K',
-        linux: 'Alt+K',
-      },
-    },
+  content_security_policy: {
+    extension_pages: "script-src 'self'; object-src 'self'",
   },
 })
