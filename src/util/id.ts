f48d4fc10a0529a401792f5419104ed6af6b2a02
Hans Meyer <hansifer@users.noreply.github.com>
2024-09-09 10:29:57 -0600

    add comment

--- src/util/id.ts
+++ src/util/id.ts
@@ -1,5 +1,7 @@
 import { customAlphabet } from 'nanoid'
 
+// do not update to allow punctuation characters
+
 export const newId = customAlphabet(
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
   6,
