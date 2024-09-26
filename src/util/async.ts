44811bbe6775d89d3db6437d7c0239c013584aa6
Hans Meyer <hansifer@users.noreply.github.com>
2024-09-25 20:09:04 -0600

    add serializer

--- src/util/async.ts
+++ src/util/async.ts
@@ -33,3 +33,19 @@ export function wait<T>(fct?: (() => T | void) | number, ms = 0): Promise<T | vo
     }, ms)
   })
 }
+
+// returns a queue function that can be called to queue up async function calls. queued functions are called serially. if any queued function call rejects, all subsequent currently-queued calls reject also. each queued call receives the resolved value of the previous queued call (or undefined for first enqueued calls). use serialize call with promise chaining (`serialize(queuedCall).then().catch()`) instead of await unless you want to wait for the queue to be exhausted.
+export function serializer() {
+  let prevPromise: Promise<any>
+  let callCount = 0
+
+  return <T extends any>(queuedCall: (lastReturnValue: T) => Promise<T>): Promise<T> => {
+    if (!callCount) prevPromise = Promise.resolve() // queue is empty
+
+    callCount++
+
+    return (prevPromise = prevPromise.then(queuedCall).finally(() => {
+      callCount--
+    }))
+  }
+}
