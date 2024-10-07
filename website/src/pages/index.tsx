807949ea46d69af7d78975752c922b42add4667f
Hans Meyer <hansifer@users.noreply.github.com>
2024-10-07 15:30:00 -0600

    remove extra page; formatting

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -10,6 +10,7 @@ import styles from './index.module.css'
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -35,6 +36,7 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
+
   return (
     <Layout
       // title={`Hello from ${siteConfig.title}`}
