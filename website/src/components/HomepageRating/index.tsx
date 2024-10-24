d464bcca98430da47b4a448db54a35b62c49a6a8
Hans Meyer <hansifer@users.noreply.github.com>
2024-10-24 13:42:42 -0600

    add HomepageRating comp

---
+++ website/src/components/HomepageRating/index.tsx
@@ -0,0 +1,18 @@
+import Heading from '@theme/Heading'
+import Stars from '@site/src/components/Stars'
+import styles from './styles.module.css'
+
+export default function HomepageRating({
+  rating,
+  text,
+}: {
+  rating: number
+  text: string
+}): JSX.Element {
+  return (
+    <section className={styles.HomepageRating}>
+      <Stars rating={rating} />
+      <Heading as="h2">{text}</Heading>
+    </section>
+  )
+}
