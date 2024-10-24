6c214f9e835e916e1fbaeaf8532219a3e6dc6e34
Hans Meyer <hansifer@users.noreply.github.com>
2024-10-24 13:49:12 -0600

    add HomepageTestimonials comp

---
+++ website/src/components/HomepageTestimonials/index.tsx
@@ -0,0 +1,12 @@
+import Heading from '@theme/Heading'
+import Testimonials from '@site/src/components/Testimonials'
+import styles from './styles.module.css'
+
+export default function HomepageTestimonials({ text }: { text: string }): JSX.Element {
+  return (
+    <section className={styles.HomepageTestimonials}>
+      <Heading as="h2">{text}</Heading>
+      <Testimonials />
+    </section>
+  )
+}
