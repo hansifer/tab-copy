dd8399c2d53520e0b3da517883f2b5e70c91bff2
Hans Meyer <hansifer@users.noreply.github.com>
2024-10-28 16:07:58 -0600

    add showHeader prop

--- website/src/components/CryptoAddress/index.tsx
+++ website/src/components/CryptoAddress/index.tsx
@@ -1,28 +1,37 @@
 import useBaseUrl from '@docusaurus/useBaseUrl'
 import styles from './styles.module.css'
 
+// todo: click to copy address to clipboard
+
 export default function CryptoAddress({
   currency,
   address,
   qrCode,
+  showHeader,
   className,
 }: {
   currency: string
   address?: string
   qrCode?: string
+  showHeader?: boolean
   className?: string
 }): JSX.Element {
   return (
     <div className={className}>
       <div className={styles.CryptoAddress}>
-        <h3>{currency}</h3>
+        {showHeader ? <h3>{currency}</h3> : null}
         {qrCode ? (
           <img
             alt={`${currency} QR code`}
             src={useBaseUrl(qrCode)}
           />
         ) : null}
-        {address ? <code>{address}</code> : null}
+        {address ? (
+          <div className={styles.address}>
+            <strong>Address:</strong>
+            <code>{address}</code>
+          </div>
+        ) : null}
       </div>
     </div>
   )
