59e8bcfbaae13d64738a91ed7109a193c158a335
Hans Meyer <hansifer@users.noreply.github.com>
2024-11-10 14:04:29 -0700

    add icon comps

---
+++ src/icons/Info.tsx
@@ -0,0 +1,27 @@
+type InfoProps = {
+  size?: number
+}
+
+export const Info = ({ size = 16 }: InfoProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="10"
+      />
+      <path d="M12 16v-4" />
+      <path d="M12 8h.01" />
+    </svg>
+  )
+}
