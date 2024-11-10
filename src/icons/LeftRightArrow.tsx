43d0ca4c4f967b9998c52a8a2bd8ef59389e4792
Hans Meyer <hansifer@users.noreply.github.com>
2024-11-10 14:46:09 -0700

    add icon comps

---
+++ src/icons/LeftRightArrow.tsx
@@ -0,0 +1,28 @@
+type LeftRightArrowProps = {
+  size?: number
+}
+
+export const LeftRightArrow = ({ size = 16 }: LeftRightArrowProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="18 8 22 12 18 16" />
+      <polyline points="6 8 2 12 6 16" />
+      <line
+        x1="2"
+        x2="22"
+        y1="12"
+        y2="12"
+      />
+    </svg>
+  )
+}
---
+++ src/icons/UpDownArrows.tsx
@@ -0,0 +1,24 @@
+type UpDownArrowsProps = {
+  size?: number
+}
+
+export const UpDownArrows = ({ size = 16 }: UpDownArrowsProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m3 16 4 4 4-4" />
+      <path d="M7 20V4" />
+      <path d="m21 8-4-4-4 4" />
+      <path d="M17 4v16" />
+    </svg>
+  )
+}
