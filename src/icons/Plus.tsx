93757b944290c13a8a6a04c1de599b33a7224ba9
Hans Meyer <hansifer@users.noreply.github.com>
2024-11-10 17:16:42 -0700

    add icon comps

---
+++ src/icons/Plus.tsx
@@ -0,0 +1,22 @@
+type PlusProps = {
+  size?: number
+}
+
+export const Plus = ({ size = 16 }: PlusProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M5 12h14" />
+      <path d="M12 5v14" />
+    </svg>
+  )
+}
